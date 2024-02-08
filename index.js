7dc9633f5959aad621b8df1075b9aa1df3a911f8
ShadowGhost25 <ilyushavavi@gmail.com>
2024-02-08 17:12:41 +0300

    registor and login

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import { validationResult } from 'express-validator';
 import { registerValidator } from './validations/auth.js'
 
 import userModel from './models/User.js'
+import cheakAuth from './utils/cheakAuth.js'
 
 mongoose
   .connect('mongodb+srv://admin:wwwwww@practic.gpq4sx8.mongodb.net/blog?retryWrites=true&w=majority')
@@ -17,6 +18,40 @@ const app = express();
 
 app.use(express.json());
 
+app.post('/auth/login', registerValidator, async (req, res) => {
+  try {
+    const user = await userModel.findOne({ email: req.body.email })
+
+    if (!user) {
+      return res.status(400).json({
+        message: 'Неверные логин или пароль'
+      })
+    }
+
+    const isValidationPass = await bcrypt.compare(req.body.password, user._doc.password)
+
+    if (!isValidationPass) {
+      return res.status(400).json({
+        message: 'Неверные логин или пароль'
+      })
+    }
+
+    const token = jwt.sign({
+      _id: user._id,
+    }, 'secret123', {
+      expiresIn: '30d',
+    })
+
+    const { passwordHash, ...userData } = user._doc
+
+    res.json({ ...userData, token })
+  } catch (error) {
+    console.log("err => ", error)
+    res.status(500).json({
+      message: "Не удалось авторизироваться"
+    })
+  }
+})
 
 app.post('/auth/register', registerValidator, async (req, res) => {
   try {
@@ -26,13 +61,13 @@ app.post('/auth/register', registerValidator, async (req, res) => {
     }
 
     const salt = await bcrypt.genSalt(10);
-    const password = await bcrypt.hash(req.body.password, salt)
+    const password = await bcrypt.hash(req.body.password, salt);
 
     const doc = new userModel({
       email: req.body.email,
       fullName: req.body.fullName,
       avatarUrl: req.body.avatarUrl,
-      password
+      password,
     })
     const user = await doc.save();
 
@@ -48,7 +83,30 @@ app.post('/auth/register', registerValidator, async (req, res) => {
   } catch (error) {
     console.log("err => ", error)
     res.status(500).json({
-      message: "НЕ удалость зарегистрироваться"
+      message: "не удалость зарегистрироваться"
+    })
+  }
+})
+
+app.get('/auth/me', cheakAuth, async (req, res) => {
+  try {
+    res.json({ 
+      success: true,
+     })
+    // const user = await userModel.findById(req.userId)
+
+    // if (!user) {
+    //   return res.status(404).json({
+    //     message: 'Пользователь не найден'
+    //   })
+    // }
+    // const { passwordHash, ...userData } = user._doc
+
+    // res.json({ userData })
+  } catch (error) {
+    console.log("err => ", error)
+    res.status(500).json({
+      message: "Нет доступа"
     })
   }
 })
